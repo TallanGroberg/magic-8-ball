df9730032cd8fc7d0125398423aa74849a186688
tallan groberg <tallan.taven@gmail.com>
2019-05-09 17:27:00 -0600

    working on bugs

--- mag8.rb
+++ mag8.rb
@@ -1,24 +1,40 @@
 # find random number generator code
+class FortuneTeller
+require 'facets'
 require 'pry'
-def menu
+require "colorize"
+def initialize
 @answer = ["yes", "no", "maybe", "only if your lucky"]
 @eightball = @answer[rand(@answer.length)]
 
-puts "im a magic 8 ball ask me anything"
-puts "press 1) if you want to know your fortune now"
-puts "press 2) if you want me to tell you what you want to hear"
-input = gets.to_i
+@new_answer = []
+@answer_clone = @answer.clone
+@all_answer = @answer << @new_answer << @answer_clone
+menu
+end 
+
+def menu
+  puts "im a magic 8 ball ask me anything".colorize(:cyan)
+  puts " 1) if you want to know your fortune now".colorize(:blue)
+puts " 2) if you want me to tell you what you want to hear".colorize(:red)
+puts " 3) to see all the answers I have."
+puts "type quit to exit"
+input = gets.strip.downcase
   case input
-    when 1
+    when "1"
+      sleep(0.5)
     ask_question
 puts @eightball
-    when 2
+    when "2"
+      sleep(0.5)
       make_answer
-    when "exit" 
-      puts "goodbye"    
+    when "3"
+      puts display
+    when "quit"
       exit
     else
-      puts "invaid input"
+      puts "invaid input".colorize(:red)
+      menu
     
   end
 end
@@ -28,32 +44,58 @@ end
 #method section
 #1
 def ask_question
+  
   puts "ask a yes or no question"
-   question = gets
+  question = gets
+  thinking
    puts @eightball
+   sleep(2)
 menu
 end
 
 #2
 def make_answer
   puts "make a new answer"
-  new_answer = gets.to_s
-  @answer << new_answer
-  puts "your new answer is (#{new_answer})"
+  @new_answer = gets.to_s
+  binding.pry
+  @new_answer << @answer_clone
+  if_there
+  
+
+  puts "your new answers are #{display}"
   
   menu
   
 end
 
-#quit 
-def halt
-  quit = gets.to_s
-  if quit == "quit"
-    exit
+def display
+  @answer_clone.each do |i|
+    puts i 
+    sleep(0.75)
+  end
+end
+
+
+def thinking
+  thinking = ["hum.", "..", "...", "....",]
+  thinking.each do |n|
+    puts n
+    sleep(0.5)
+  end
+end
+
+def if_there
+  if @new_answers.frequency == @answer.frequency 
+    puts "already an answer"
   else 
-    menu
+    @new_answer << @answer 
+  end
+
+  end
+  def everything
+    
   end
 
 end
-menu
 
+FortuneTeller.new

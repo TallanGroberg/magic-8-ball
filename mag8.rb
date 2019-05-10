f1c3467cf6f92437b87469dd23bc6f668012a20e
tallan groberg <tallan.taven@gmail.com>
2019-05-09 23:11:02 -0600

    tracking bug

--- mag8.rb
+++ mag8.rb
@@ -7,19 +7,19 @@ def initialize
 @answer = ["yes", "no", "maybe", "only if your lucky"]
 @eightball = @answer[rand(@answer.length)]
 
-@new_answer = []
+# @new_answer = []
 @answer_clone = @answer.clone
-@all_answer = @answer << @new_answer << @answer_clone
+@all_answer = @answer << @new_answer  # << @answer_clone
 menu
 end 
 
 def menu
   puts "im a magic 8 ball ask me anything".colorize(:cyan)
   puts " 1) if you want to know your fortune now".colorize(:blue)
-puts " 2) if you want me to tell you what you want to hear".colorize(:red)
-puts " 3) to see all the answers I have."
-puts "type quit to exit"
-input = gets.strip.downcase
+  puts " 2) if you want me to tell you what you want to hear".colorize(:red)
+  puts " 3) to see all the answers I have."
+  puts "type quit to exit"
+  input = gets.strip.downcase
   case input
     when "1"
       sleep(0.5)
@@ -56,12 +56,9 @@ end
 #2
 def make_answer
   puts "make a new answer"
-  @new_answer = gets.to_s
-  binding.pry
-  @new_answer << @answer_clone
-  if_there
-  
-
+  @new_answer = gets
+  @new_answer << @answer
+  # if_there
   puts "your new answers are #{display}"
   
   menu
@@ -73,6 +70,7 @@ def display
     puts i 
     sleep(0.75)
   end
+  menu
 end
 
 
@@ -86,14 +84,12 @@ end
 
 def if_there
   if @new_answers.frequency == @answer.frequency 
+    binding.pry
     puts "already an answer"
   else 
     @new_answer << @answer 
   end
 
-  end
-  def everything
-    
   end
 
 end
